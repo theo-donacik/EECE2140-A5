9922fcaab6279f5e3af9112524d7bca5f1945f80
The0919 <theodonacik@gmail.com>
2024-02-29 18:31:18 -0500

    Class and first function added

---
+++ Assignment05_Donacik.py
@@ -0,0 +1,14 @@
+class BasicMathOperations:
+  def greet(self, first, last):
+    """Prints a greeting with first and last name.
+    
+    Args:
+        first (string): The first name.
+        number2 (string): The last name.
+        
+    Returns:
+        void: No return but prints the output
+    """
+    print("Hello", first, last + "!")
+
+  
