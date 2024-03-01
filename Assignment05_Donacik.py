df7697ade727b1aec515afa3fffa630d833adc7f
The0919 <theodonacik@gmail.com>
2024-02-29 20:59:05 -0500

    added 3 more functions

--- Assignment05_Donacik.py
+++ Assignment05_Donacik.py
@@ -55,6 +55,44 @@ class BasicMathOperations:
     Returns:
         int/float: The square of num
     """
-    return num ** 2    
+    return num ** 2
+
+  def factorial(self, num):
+    """Gets the factorial of num
+    
+    Args:
+        num (int/float): The number.
+                
+    Returns:
+        int/float: The factorial of num
+    """
+    fact = 1
+    for i in range(num):
+      fact *= i+1
+    return fact
+
+  def counting(self, start, end):
+    """Prints count from start to end
+    
+    Args:
+        start (int/float): The starting number.
+        end (int/float): The ending number.
+                
+    Returns:
+        void: No return but prints the result
+    """
+    print(*[*range(start, end+1)])
+
+
+  def calculateHypotenuse(self, base, perpindicular):
+    """Gets the hypotenuse of a right angle triangle given the base and perpindicular
+    Args:
+        base (int/float): The base length of the triangle.
+        perpindicular (int/float): The base length of the triangle.
+                
+    Returns:
+        int/float: The hypotenuse 
+    """
+    return ((self.calculateSquare(base) + self.calculateSquare(perpindicular)) ** .5)
 
 
